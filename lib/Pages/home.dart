6ccb1f46e39b897adfada66ad76f2f7a66f59e16
Josoa Vonjiniaina <76913187+Josoa886@users.noreply.github.com>
2024-01-07 22:28:27 +0300

    Updating Home

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -11,6 +11,27 @@ class Home extends StatelessWidget{
     return Scaffold(
       appBar: AppBar(
         title: Text("Homepage", style: TextStyle(fontSize: 20, color: Colors.white),),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      extendBodyBehindAppBar: false,
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+        child: Center(
+          child: Column(
+            children: [
+              Text("Hello, your're in the homepage"),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        width: 50,
+        height: 60,
+        decoration: BoxDecoration(
+          color: Colors.blueAccent,
+        ),
+        //child: ,
       ),
       //bottomNavigationBar: ,
     );
