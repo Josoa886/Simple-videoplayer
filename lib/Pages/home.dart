adea56b2a36207db7107695eb8031a6c5430d6bd
Josoa Vonjiniaina <76913187+Josoa886@users.noreply.github.com>
2024-01-11 11:20:13 +0300

    Adding List on the Homepage

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -1,8 +1,8 @@
 
 import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:videoplayer/Pages/files.dart';
 import 'package:videoplayer/library/bottom_navbar.dart';
@@ -23,23 +23,109 @@ class Home extends StatelessWidget{
         centerTitle: true,
       ),
       extendBodyBehindAppBar: false,
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
-        child: const Center(
-          child: Column(
-            children: [
-              Text("Hello, you're in the homepage"),
-              SizedBox(
-                width: 50,
-                height: 70,
-              ),
-              SizedBox(
-                width: 100,
-                height: 600,)
-            ],
-          ),
-        ),
-      ),
+      body: ListView(
+        padding: EdgeInsets.only(left: 15, right: 15),
+                children: [
+                  const Text(
+                    "Items A",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700
+                    ),),
+                 SizedBox(
+                   height: 150,
+                   child: ListView.builder(
+                     scrollDirection: Axis.horizontal,
+                       itemCount: 10,
+                       itemBuilder: (context, index){
+                         return GestureDetector(
+                           onTap: (){},
+                           child: Container(
+                             margin: const EdgeInsets.only(left: 10, right: 10),
+                             alignment: Alignment.center,
+                             width: 100,
+                             decoration: BoxDecoration(
+                               color: Colors.blueAccent,
+                               borderRadius: BorderRadius.circular(10),
+                             ),
+                             child: Text("${index +1}"),
+                           ),
+                         );
+                       }
+                   ),
+                 ),
+
+                  const SizedBox( height: 30,),
+                  const Text(
+                    "Items B",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700
+                    ),),
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+                    height: 150,
+                    color: Colors.grey,),
+
+
+                  const SizedBox( height: 30,),
+                  const Text(
+                    "Items C",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700
+                    ),),
+                  SizedBox(
+                    height: 250,
+                    child: ListView.builder(
+                        scrollDirection: Axis.vertical,
+                        itemCount: 5,
+                        itemBuilder: (context, index){
+                          return GestureDetector(
+                            onTap: (){},
+                            child: Container(
+                              margin: const EdgeInsets.only(bottom: 10,),
+                              alignment: Alignment.center,
+                              height: 80,
+                              decoration: BoxDecoration(
+                                color: Colors.deepPurpleAccent,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Text("${index +1}"),
+                            ),
+                          );
+                        }
+                    ),
+                  ),
+
+                  const SizedBox( height: 30,),
+                  const Text(
+                    "Items D",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700
+                    ),),
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+                    height: 200,
+                    color: Colors.green,
+                  ),
+
+
+                  const SizedBox( height: 30,),
+                  const Text(
+                    "Items E",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700
+                    ),),
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+                    height: 200,
+                    color: Colors.green,
+                  )
+                ],
+              )
     );
   }
 }
